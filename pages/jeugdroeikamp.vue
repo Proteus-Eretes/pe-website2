bb7a4595d6373b81ea7005d49951c7ba86f9c024
Gerben Mulder <gerbenmulder2508@gmail.com>
2023-03-27 21:55:33 +0200

    feat: add jeugdroeikamp page

---
+++ pages/jeugdroeikamp.vue
@@ -0,0 +1,289 @@
+<template>
+  <div class="bg-[url('/jeugdroeikamp/achtergrondflyer.jpg')] bg-repeat bg-black bg-contain">
+    <header>
+      <nav class="hide-on-med-and-up">
+        <div class="nav-wrapper">
+          <a href="www.proteus-eretes.nl" class="brand-logo">Jeugdroeikamp</a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul class="side-nav" id="mobile-demo">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#kamp">Kamp</a></li>
+            <li><a href="#wat">Wat?</a></li>
+            <li><a href="#wie">Voor wie?</a></li>
+            <li><a href="#ouders">Voor ouders</a></li>
+            <li><a href="#enthousiast">Enthousiast (inschrijven!)</a></li>
+            <li><a href="#vragen">Vragen?</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <main>
+      <div class="container">
+        <div class="row">
+          <div class="col s12 m9 l10">
+            <div id="home" class="section scrollspy">
+              <div id="topSlider" class="slider center-align">
+                <ul class="slides">
+                  <li>
+                    <img src="/jeugdroeikamp/gastmetboot.jpg">
+                  </li>
+                  <li>
+                    <img src="/jeugdroeikamp/kampvuur.jpg">
+                  </li>
+                  <li>
+                    <img src="/jeugdroeikamp/botenopwater.jpg">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <div id="kamp" class="scrollspy">
+              <h1 class="h1-title center-align">Junioren<wbr>roei&shy;kamp<span class="text-orange">.</span></h1>
+              <div class="block-grey">
+                <a href="/jeugdroeikamp/kamp.jpg" target="_blank">
+                  <img src="/jeugdroeikamp/kamp.jpg" class="responsive-img small-img left">
+                </a>
+                <p>
+                  Aankomende 17 tot en met 21 juli zal het Proteus-Eretes juniorenroeikamp plaatsvinden. 
+                  Een zomerkamp speciaal voor jeugdroeiers, met allerlei leuke activiteiten en natuurlijk roeien!
+                </p>
+                <p>
+                  Het kamp zal plaatsvinden op het terrein van Scoutcentrum het Kruithuis, tegenover De Delftse 
+                  Studentenvereniging Proteus-Eretes. Voor het roeien zullen we gebruik maken van de faciliteiten 
+                  van Proteus-Eretes.
+                </p>
+              </div>
+            </div>
+
+            <div id="wat" class="scrollspy">
+              <h1 class="center-align">Wat<span class="text-orange">?</span></h1>
+              <div class="block-grey">
+                <a href="/jeugdroeikamp/wat.jpg" target="_blank">
+                  <img src="/jeugdroeikamp/wat.jpg" class="responsive-img small-img right">
+                </a>
+                <p>
+                  Tijdens het zomerkamp zullen we ons natuurlijk elke dag bezig houden met roeien, 
+                  maar is er ook vooral veel ruimte voor andere leuke activiteiten of uitjes naar 
+                  andere plekken in de omgeving (zoals het strand). Tijdens het roeien zullen we zo 
+                  veel mogelijk bootjes maken die van gelijk niveau is zodat er onderling een beetje 
+                  gespard kan worden. Het zal dus de ideale plek worden om beter kennis te leren maken 
+                  met andere jeugdroeiers uit Nederland!
+                </p>
+                <p>
+                  Tijdens het kamp zullen we voor het grootste deel verblijven op het terrein van Scoutcentrum 
+                  het Kruithuis. Wij zullen hier ook overnachten. Het terrein bevindt zich aan de andere kant 
+                  van de Schie tegenover het gebouw van Proteus-Eretes. Voor het roeien zullen we gebruik maken 
+                  van de faciliteiten van Proteus-Eretes.
+                </p>
+              </div>
+            </div>
+
+            <div id="wie" class="scrollspy">
+              <h1 class="center-align">Voor wie<span class="text-orange">?</span></h1>
+              <div class="block-grey">
+                <a href="/jeugdroeikamp/wie.jpg" target="_blank">
+                  <img src="/jeugdroeikamp/wie.jpg" class="responsive-img small-img left">
+                </a>
+                <p>
+                  Alle enthousiaste jeugdroeiers met minimaal een jaar roeiervaring zijn welkom! Speciaal richten wij ons op
+                  actieve roeiers <b>rond de 14-19 jaar</b>. Weet je niet zeker of het kamp
+                  geschikt is voor je, neem dan gerust contact met ons op.
+                </p>
+                <p>
+                  Je bent van harte welkom om met je hele ploeg te komen, maar ook in je eentje zal je het ontzettend naar je zin hebben.
+                  Doordat de deelnemers van verschillende verenigingen, leeftijden en niveaus gemengd zullen zijn, ontstaat er een gezellige groep.
+                </p>
+              </div>
+            </div>
+
+            <div id="ouders" class="scrollspy">
+              <h1 class="center-align">Voor ouders<span class="text-orange">.</span></h1>
+              <div class="block-grey">
+                <p>
+                  Op het kamp zal veel begeleiding aanwezig zijn om ervoor te zorgen
+                  dat uw dochter/zoon ten alle tijden veilig is en op zijn/haar gemak.
+                  Tijdens  de activiteiten en het roeien zal er altijd begeleiding aanwezig zijn om
+                  ervoor te zorgen dat de activiteiten en het roeien niet alleen leuk en leerzaam is,
+                  maar ook veilig. Verder vinden wij het belangrijk dat er direct contact
+                  is tussen de ouders en de organisatie van het kamp. Daarom zullen wij
+                  bij het inschrijfformulier om uw telefoonnummer en emailadres vragen,
+                  zodat we u op de hoogte kunnen houden indien dit nodig is. Andersom
+                  vinden wij het heel belangrijk dat ouders hun junior met een gerust hart
+                  naar het trainingskamp kunnen sturen. Als u dus nog
+                  vragen heeft, kunt u ons een mailtje sturen via
+                  <a href="mailto:jojo@proteus-eretes.nl">jojo@proteus-eretes.nl</a>.
+                  De geschatte kosten voor het kamp bedragen momenteel &euro;250,-.
+                  Eind mei zullen de definitieve kosten voor het kamp bekend gemaakt worden!
+                </p>
+              </div>
+            </div>
+
+            <div id="enthousiast" class="scrollspy">
+              <h1 class="center-align">Enthousiast<span class="text-orange">!</span></h1>
+              <div class="block-grey">
+                <p>
+                  Ben je enthousiast? Schrijf je dan <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMn_ADM5ycA-DZYpuFJqm97FXTAnKiw-wmLnffLseSkuRnow/viewform" target="blank">hier</a> meteen in!
+                </p>
+              </div>
+            </div>
+
+            <div id="vragen" class="scrollspy">
+              <h1 class="center-align">Vragen<span class="text-orange">?</span></h1>
+              <div class="block-grey">
+                <p>
+                  Toch nog vragen? Communicatie kamp, Paul, is te bereiken via mobiele telefoon
+                  (<a href="tel:+316-12097849">+316-12097849</a>). Je kan ook een mailtje sturen naar
+                  <a href="mailto:jojo@proteus-eretes.nl">jojo@proteus-eretes.nl</a>.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col hide-on-small-only m3 l2">
+            <div style="position:fixed;top:80px;">
+              <ul class="table-of-contents">
+                <li><a id="inschrijfButton" class="btn waves-effect waves-light">Inschrijven!</a></li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#kamp">Kamp</a></li>
+                <li><a href="#wat">Wat?</a></li>
+                <li><a href="#wie">Voor wie?</a></li>
+                <li><a href="#ouders">Voor ouders</a></li>
+                <li><a href="#enthousiast">Enthousiast</a></li>
+                <li><a href="#vragen">Vragen?</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+@font-face {
+    font-family: HelveticaProteus;
+    src: url("/helveticaproteus/HelveticaProteusEretes-Roman.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: HelveticaProteus;
+    src: url("/helveticaproteus/HelveticaProteusEretes-Roman_Italic.otf") format("opentype");
+    font-weight: normal;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: HelveticaProteus;
+    src: url("/helveticaproteus/HelveticaProteusEretes.otf") format("opentype");
+    font-weight: bold;
+    font-style: normal;
+}
+
+h1 {
+    font-weight: bolder;
+    color: #B2C3D1;
+
+    line-height: 80%;
+    margin-bottom: -0.075em;
+}
+
+h1.h1-title {
+    text-transform: uppercase;
+}
+
+@media only screen and (max-width: 600px) {
+    h1 {
+        font-size: 3rem;
+    }
+}
+
+p {
+    text-align: justify;
+}
+
+.text-orange {
+    color: #F57F38;
+}
+
+.row a {
+    color: inherit;
+    font-style: italic;
+    transition: color .3s;
+}
+
+.row a:hover {
+    color: #F57F38;
+    transition: color .15s;
+}
+
+.block-grey {
+    background-color: rgb(200, 200, 200);
+    background-color: rgba(255, 255, 255, 0.5);
+    padding: 10px 10px 15px;
+    overflow: auto;
+}
+
+nav {
+    background-color: #6285A3;
+    line-height: 64px;
+    height: 64px;
+}
+
+.img-logo { /* Proteus logo in navbar */
+    display: block;
+    height: 60px;
+    padding-top: 3px;
+}
+
+.page-footer {
+    background-color: #6285A3;
+}
+
+.table-of-contents a {
+    color: lightgray;
+    color: rgba(255, 255, 255, 0.8);
+    font-style: normal;
+}
+
+.table-of-contents a.active {
+    border-left-color: #F57F38;
+}
+
+.table-of-contents a:hover {
+    border-left-color: #F57F38;
+}
+
+.table-of-contents a.btn,
+.table-of-contents a.btn:hover {
+    margin-bottom: 5px;
+    border: none;
+    color: white;
+    background-color: #F57F38;
+}
+
+.slider .indicators .indicator-item.active {
+    background-color: #F57F38;
+}
+
+.carousel-iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+}
+
+@media only screen and (min-width: 601px) {
+    .small-img.right {
+        width: 40%;
+        padding-left: 22px;
+    }
+
+    .small-img.left {
+        width: 40%;
+        padding-right: 22px;
+    }
+}
+</style>
